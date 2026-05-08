01e16498d156e2ef37ce20b5bffd0053f6d6f601
Pandey Abhigyan Prakash <officialabhigyanpp@gmail.com>
2026-05-08 09:57:51 +0530

    Fixed folder structure and added basics

---
+++ 01-BASICS/basics.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+
+int main() {
+    std::cout << "Hey Abhigyan!" << "\n";
+    std::cout << "Hey Abhigyan!"
+    return 0;
+}
---
+++ victory.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Entrance exams are over. Engineering begins now." << std::endl;
+    std::cout << "Pandey Abhigyan Prakash - Mac Neo - May 4, 2026" << std::endl;
+    return 0;
+}
